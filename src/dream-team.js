a3c26e57e5a3df06757840baddf2ae2b23b19e14
Veronika <verona.binko@gmail.com>
2021-03-30 22:28:26 +0300

    feat: complete task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+    let arr = [];
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        let str = members[i].trim();
+        arr.push(str[0].toUpperCase());
+      }
+    }
+  return arr.sort().join('');
 };
